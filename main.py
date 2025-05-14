d508e23dc46163f382c57e594ca58652651fabc2
hmontanaro <hazael.montanaro@empa.ch>
2025-05-14 15:29:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# %%
+# !%reset
+
+# %%
+# ! %load_ext autoreload
+# ! %autoreload 2
+
+# %%
+# ! %matplotlib widget
+
+# %%
+import os
+print(os.getcwd())
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
